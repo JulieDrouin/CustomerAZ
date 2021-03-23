72c1015f048fc56c9faaa8660d0dabf93b218866
Julie Jourdrouin <jourdrouin.julie@gmail.com>
2021-03-23 11:33:14 +0100

    feat: router

--- app-crud-customer/src/App.js
+++ app-crud-customer/src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import { BrowserRouter } from 'react-router-dom';
 import Header from "./components/Header/Header";
+import AllRoutes from "./components/router/AllRoutes";
 
 const App = () => {
   const titre= "CLIENTS";
@@ -9,6 +10,7 @@ const App = () => {
       <div className="App">
         <Header titre={titre}/>
         <div className="container-fluid px-0">
+          <AllRoutes />
         </div>
       </div>
     </BrowserRouter>
---
+++ app-crud-customer/src/components/router/AllRoutes.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { Switch, Route } from "react-router-dom";;
+
+const AllRoutes = (props) => {
+  return (
+    <Switch>
+    </Switch>
+  );
+};
+export default AllRoutes;
